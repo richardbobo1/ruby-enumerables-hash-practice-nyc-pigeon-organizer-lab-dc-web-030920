28bbe84a0b9254b230e95929e0743de705ee43ed
Richard Bobo <richard.bobo@gmail.com>
2020-03-08 16:40:23 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,17 +3,21 @@ def nyc_pigeon_organizer(data)
   
   data.each do |color_gender_lives, value|
     value.each do |stats, all_names|
-      all_names.each do |name|
+      all_names.each do |name|   #zeros in on names 
         
-        if pigeon_list[name] == nil
-          pigeon_list[name] = {}
+        if pigeon_list[name] == nil   #if not in  new hash 
+          pigeon_list[name] = {}     #add to hash key 
         end
         
         if pigeon_list[name][color_gender_lives] == nil
           pigeon_list[name][color_gender_lives] = []
           
+              #if current characterist isn't present for 
+              #for current pigon, then add it 
         end
         pigeon_list[name][color_gender_lives].push(stats.to_s)
+        
+            #push the characteristics as strings. 
         
       end
     end 
